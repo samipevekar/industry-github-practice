fa57785795e2aafce72d1e1c4a0042b992051039
samipevekar <samipevekar499@gmail.com>
2026-01-17 12:24:04 +0530

    no

--- add.js
+++
@@ -1,6 +0,0 @@
-function add(a, b) {
-  return a + b + 1;
-}
-
-
-module.exports = add;
--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 function add(a, b) {
-  let num = a+b
-  return num;
+  return a + b;
 }
 
-function isEven(num) {
-  return num % 2 === 0;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-module.exports = { add, isEven, multiply };
+module.exports = add;
